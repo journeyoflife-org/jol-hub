e346ea3e472c79b022bf7f70b5c9ab9654e310a1
Mr. Gintaras Kazlauskas <journey4oflife@gmail.com>
2026-09-22 21:44:03 +0300

    fix(test_middleware.py): isort import ordering

--- backend/django/apps/tenants/tests/test_middleware.py
+++ backend/django/apps/tenants/tests/test_middleware.py
@@ -6,6 +6,7 @@ import uuid
 from unittest.mock import MagicMock, patch
 
 import pytest
+
 from apps.tenants.test_utils import clear_test_tenant_context, set_test_tenant_context
 from django.http import HttpResponse
 from django.test import RequestFactory
